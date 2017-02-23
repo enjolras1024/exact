08e2461fb04e2c2b4054a35cee7e63e726c18732
enjolras1024 <enjolras1024@163.com>
2017-02-23 18:46:20 +0800

    @0.0.8

--- examples/color-palette/app.js
+++ examples/color-palette/app.js
@@ -1,107 +1,109 @@
-var Skin = Exact.Skin;
-var Component = Exact.Component;
+(function() {
+  var Skin = Exact.Skin;
+  var Component = Exact.Component;
 
-function ChannelEditor() {
-  Component.apply(this, arguments);
-}
-
-Exact.defineClass({
-  constructor: ChannelEditor, extend: Component,
-
-  statics: {
-    descriptors: ['label', 'value'],
-    template: Skin.query(document, '.template .channel-editor')
-  },
-
-  register: function() {
-    Exact.help(this).bind('onChange', 'onSlide');
-  },
-
-  onChange: function(event) {
-    var value = Number(event.target.value);
-    if (!isNaN(value)) {
-      this.set('value', value);
-    }
-  },
-
-  onSlide: function(event) {
-    this.set('value', event.target.value);
+  function ChannelEditor() {
+    Component.apply(this, arguments);
   }
-});
 
-function clip(value) {
-  value = Number(value);
-  return value < 0 ? 0 : (value > 255 ? 255 : value);
-}
+  Exact.defineClass({
+    constructor: ChannelEditor, extend: Component,
 
-function dec2hex(n) {
-  var s = Number(n).toString(16);
-  return  s.length > 1 ? s : '0' + s;
-}
-
-function hex2dec(s) {
-  return Number('0x' + s);
-}
-
-function Palette() {
-  Component.apply(this, arguments);
-}
-
-Exact.defineClass({
-  constructor: Palette, extend: Component,
-
-  statics: {
-    template: Skin.query(document, '.template .palette'),
-
-    resources: {
-      ChannelEditor: ChannelEditor
+    statics: {
+      descriptors: ['label', 'value'],
+      template: Skin.query(document, '.template .channel-editor')
     },
 
-    descriptors: {
-      red: {
-        coerce: clip
+    register: function() {
+      Exact.help(this).bind('onChange', 'onSlide');
+    },
+
+    onChange: function(event) {
+      var value = Number(event.target.value);
+      if (!isNaN(value)) {
+        this.set('value', value);
+      }
+    },
+
+    onSlide: function(event) {
+      this.set('value', event.target.value);
+    }
+  });
+
+  function clip(value) {
+    value = Number(value);
+    return value < 0 ? 0 : (value > 255 ? 255 : value);
+  }
+
+  function dec2hex(n) {
+    var s = Number(n).toString(16);
+    return  s.length > 1 ? s : '0' + s;
+  }
+
+  function hex2dec(s) {
+    return Number('0x' + s);
+  }
+
+  function Palette() {
+    Component.apply(this, arguments);
+  }
+
+  Exact.defineClass({
+    constructor: Palette, extend: Component,
+
+    statics: {
+      template: Skin.query(document, '.template .palette'),
+
+      resources: {
+        ChannelEditor: ChannelEditor
       },
-      blue: {
-        coerce: clip
-      },
-      green: {
-        coerce: clip
-      },
-      color: {
-        validator: /^\s*#[0-9a-fA-F]{6}\s*$/,
-        depends: ['red', 'green', 'blue'],
-        get: function() {
-          return '#' + dec2hex(this.red) + dec2hex(this.green) + dec2hex(this.blue);
+
+      descriptors: {
+        red: {
+          coerce: clip
         },
-        set: function(value) {
-          this.set('red', hex2dec(value.slice(1, 3)));
-          this.set('green', hex2dec(value.slice(3, 5)));
-          this.set('blue', hex2dec(value.slice(5, 7)));
+        blue: {
+          coerce: clip
+        },
+        green: {
+          coerce: clip
+        },
+        color: {
+          validator: /^\s*#[0-9a-fA-F]{6}\s*$/,
+          depends: ['red', 'green', 'blue'],
+          get: function() {
+            return '#' + dec2hex(this.red) + dec2hex(this.green) + dec2hex(this.blue);
+          },
+          set: function(value) {
+            this.set('red', hex2dec(value.slice(1, 3)));
+            this.set('green', hex2dec(value.slice(3, 5)));
+            this.set('blue', hex2dec(value.slice(5, 7)));
+          }
+        }
+      },
+
+      defaults: function() {
+        return {
+          red: 0,
+          blue: 0,
+          green: 0,
+          isInvalid: false
         }
       }
     },
 
-    defaults: function() {
-      return {
-        red: 0,
-        blue: 0,
-        green: 0,
-        isInvalid: false
-      }
+    register: function() {
+      Exact.help(this).bind('onChange');
+
+      this.on('validated.color', (function(event, error) {
+        this.set('isInvalid', !!error);
+      }).bind(this));
+    },
+
+    onChange: function(event) {
+      this.set('color', event.target.value);
     }
-  },
+  });
 
-  register: function() {
-    Exact.help(this).bind('onChange');
-
-    this.on('validated.color', (function(event, error) {
-      this.set('isInvalid', !!error);
-    }).bind(this));
-  },
-
-  onChange: function(event) {
-    this.set('color', event.target.value);
-  }
-});
-
-Component.create(Palette).attach(Skin.query(document, '#palette'));
+  Component.create(Palette).attach(Skin.query(document, '#palette'));
+})();
--- examples/svg-component/app.js
+++ examples/svg-component/app.js
@@ -1,103 +1,105 @@
-var Component = Exact.Component;
-var Skin = Exact.Skin;
+(function() {
+  var Component = Exact.Component;
+  var Skin = Exact.Skin;
 
-function Slider() {
-  Component.apply(this, arguments);
-}
+  function Slider() {
+    Component.apply(this, arguments);
+  }
 
-Exact.defineClass({
-  constructor: Slider, extend: Component,
-  statics: {
-    defaults: function() {
-      return {
-        min: 0,
-        max: 10,
-        value: 0
-      };
+  Exact.defineClass({
+    constructor: Slider, extend: Component,
+    statics: {
+      defaults: function() {
+        return {
+          min: 0,
+          max: 10,
+          value: 0
+        };
+      },
+      template: Skin.query(document, '.template .slider')
     },
-    template: Skin.query(document, '.template .slider')
-  },
-  onMouseChange: function(event) {
-    switch (event.type) {
-      case 'mousemove':
-        if (this.pressed) {
-          var max = Number(this.max);
-          var min = Number(this.min);
-          //var cx = event.clientX - this.$skin.getBoundingClientRect().left;
-          var cx = event.clientX - Skin.call(this.$skin, 'getBoundingClientRect').left;
-          cx = cx < 0 ? 0 : cx;
-          cx = cx > 100 ? 100 : cx;
-          this.set('value', (cx * 0.01 * (max - min) + min).toFixed(1));
-        }
-        break;
-      case 'mousedown':
-        this.pressed = true;
-        break;
-      case 'mouseup':
-        this.pressed = false;
-        break;
-    }
-  },
-
-  register: function() {
-    Exact.help(this).bind('onMouseChange');
-  },
-  ready: function() {
-
-    var body = document.body;
-    body.addEventListener('mousemove', this.onMouseChange);
-    body.addEventListener('mouseup', this.onMouseChange);
-
-    var self = this;
-    this.on('changed', function(event) {
-      if (event.keyName !== 'value') {
-        var value = self.value;
-        value = value < self.min ? self.min : value;
-        value = value > self.max ? self.max : value;
-        self.set('value', value);
+    onMouseChange: function(event) {
+      switch (event.type) {
+        case 'mousemove':
+          if (this.pressed) {
+            var max = Number(this.max);
+            var min = Number(this.min);
+            //var cx = event.clientX - this.$skin.getBoundingClientRect().left;
+            var cx = event.clientX - Skin.call(this.$skin, 'getBoundingClientRect').left;
+            cx = cx < 0 ? 0 : cx;
+            cx = cx > 100 ? 100 : cx;
+            this.set('value', (cx * 0.01 * (max - min) + min).toFixed(1));
+          }
+          break;
+        case 'mousedown':
+          this.pressed = true;
+          break;
+        case 'mouseup':
+          this.pressed = false;
+          break;
       }
-    });
-  },
-  normalize: function(value) {
-    return (value - this.min) / (this.max - this.min) * 100;
-  }
-});
-
-function TextSlider() {
-  Component.apply(this, arguments);
-}
-
-Exact.defineClass({
-  constructor: TextSlider, extend: Component,
-  statics: {
-    template: Skin.query(document, '.template .text-slider'),
-    resources: {
-      Slider: Slider
     },
-    defaults: function() {
-      return {
-        min: 0,
-        max: 10,
-        value: 0,
-        label: ''
-      };
+
+    register: function() {
+      Exact.help(this).bind('onMouseChange');
+    },
+    ready: function() {
+
+      var body = document.body;
+      body.addEventListener('mousemove', this.onMouseChange);
+      body.addEventListener('mouseup', this.onMouseChange);
+
+      var self = this;
+      this.on('changed', function(event) {
+        if (event.keyName !== 'value') {
+          var value = self.value;
+          value = value < self.min ? self.min : value;
+          value = value > self.max ? self.max : value;
+          self.set('value', value);
+        }
+      });
+    },
+    normalize: function(value) {
+      return (value - this.min) / (this.max - this.min) * 100;
     }
+  });
+
+  function TextSlider() {
+    Component.apply(this, arguments);
   }
 
-});
-
-function App() {
-  Component.apply(this, arguments);
-}
-
-Exact.defineClass({
-  constructor: App, extend: Component,
-  statics: {
-    template: Skin.query(document, '.template .app'),
-    resources: {
-      TextSlider: TextSlider
+  Exact.defineClass({
+    constructor: TextSlider, extend: Component,
+    statics: {
+      template: Skin.query(document, '.template .text-slider'),
+      resources: {
+        Slider: Slider
+      },
+      defaults: function() {
+        return {
+          min: 0,
+          max: 10,
+          value: 0,
+          label: ''
+        };
+      }
     }
-  }
-});
 
-Component.create(App).attach(Skin.query(document, '#app'));
+  });
+
+  function App() {
+    Component.apply(this, arguments);
+  }
+
+  Exact.defineClass({
+    constructor: App, extend: Component,
+    statics: {
+      template: Skin.query(document, '.template .app'),
+      resources: {
+        TextSlider: TextSlider
+      }
+    }
+  });
+
+  Component.create(App).attach(Skin.query(document, '#app'));
+})();
